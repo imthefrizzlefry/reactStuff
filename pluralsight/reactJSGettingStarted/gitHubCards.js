642765380e3e5c3c8f7a73f50364b8e7f138c1db
Steven Farnell <a-sfarnell@expedia.com>
2017-09-08 13:32:28 -0700

    remove placeholder data, and fetch cards from github API

--- pluralsight/reactJSGettingStarted/gitHubCards.js
+++ pluralsight/reactJSGettingStarted/gitHubCards.js
@@ -12,21 +12,58 @@ const Card = (props) => {
     );
 };
 
-let data = [
-    {   name: "imthefrizzlefry",
-        avatar_url: "https://avatars1.githubusercontent.com/u/8007606?v=4",
-        company: "none" },
-    {   name: "someone",
-        avatar_url: "http://placehold.it/75",
-        company: "null" }
-];
-
 const CardList = (props) => {
     return (
         <div>
-            {props.cards.map(card => <Card />)}
+            {props.cards.map(card => <Card key={card.id} {...card} />)}
         </div>
     )
 }
 
-ReactDOM.render(<CardList cards={data} />, mountNode);
+class Form extends React.Component {
+    state = { userName: '' }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        axios.get(`https://api.github.com/users/${this.state.userName}`)
+            .then(resp => {
+                this.props.onSubmit(resp.data);
+                this.setState({ userName: '' });
+            });
+    };
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" 
+                value={this.state.userName}
+                onChange={(event) => this.setState({userName: event.target.value})}
+                placeholder="Github username" />
+                <button type="submit" >Add Card</button>
+            </form>
+        );        
+    }
+}
+
+class App extends React.Component {
+
+    state = {
+        cards: []
+    }
+
+    addNewCard = (cardInfo) => {
+        this.setState(prevState => ({
+            cards: prevState.cards.concat(cardInfo)
+        }));
+    };
+    
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.addNewCard}/>
+                <CardList cards={this.state.cards} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, mountNode);
